e1cb5d9e69e8be640363b7dd6e54b8a438cba36e
Lorenz Schmidt <Lorenz Schmidt>
2021-08-04 16:35:31 +0200

    Chebyshev polynomials?

--- src/model.rs
+++ src/model.rs
@@ -2,36 +2,50 @@ use ndarray::{Array1, Array2};
 use ndarray_linalg::Solve;
 use crate::params::ParamSamples;
 
+/// https://stackoverflow.com/questions/382186/fitting-polynomials-to-data
+fn poly_fnc(val: f32, n: usize) -> Vec<f32> {
+    let ln_val = val.ln();
+    (1..=n).map(|x| (x as f32) * ln_val).collect()
+}
+
+
+fn poly_fnc_deriv(val: f32, n: usize) -> Vec<f32> {
+    (1..=n).map(|x| (x as f32) / val).collect()
+}
+
 pub fn estimate_stepsize(results: &[(usize, u64)], min_change: u64) -> usize {
-    if results.len() < 2 {
-        return results[results.len()-1].0 + 1;
+    let n = results.len();
+    if n < 4 {
+        return results[n-1].0 + 1;
     }
 
-    let x = Array1::from_iter(results.iter().map(|x| x.1 as f32));
-    let d: Vec<f32> = results.iter().map(|x| {
-        let mut n = x.0 as f32 + 1.0;
-        let mut out = Vec::with_capacity(results.len());
+    let targets = Array1::from_iter(results.iter().map(|x| (x.1 as f32).ln()));
+    let polys: Vec<f32> = results.iter()
+        .map(|x| poly_fnc(x.0 as f32 + 2.0, n))
+        .flatten()
+        .collect();
 
-        for i in 0..results.len() {
-            n = n * (x.0 as f32 + 1.0);
-            out.push(n);
-        }
-
-        out
-    }).flatten().collect();
-    let d = Array2::from_shape_vec((results.len(), results.len()), d).unwrap();
+    let polys = Array2::from_shape_vec((n, n), polys).unwrap();
 
     dbg!(&results);
-    dbg!(&x, &d);
+    dbg!(&targets, &polys);
 
-
-    let beta = d.solve(&x).unwrap();
+    let beta = polys.solve(&targets).unwrap();
 
     dbg!(&beta);
 
     // perform newton-raphson
-    let next_target = results[results.len()-1] + min_change as f32;
-    let fnc = |z: f32| -> f32 { next_target -
+    let next_target = (results[n-1].1 + min_change) as f32;
+    let fnc = |z: f32| -> f32 { next_target - poly_fnc(z, n).iter().zip(beta.iter()).map(|(a, b)| a*b).sum::<f32>() };
+    let d_fnc = |z: f32| -> f32 { - poly_fnc_deriv(z, n).iter().zip(beta.iter()).map(|(a, b)| a*b).sum::<f32>() };
+
+    panic!("");
+    let mut z = results[n-1].0 as f32;
+    dbg!(&next_target);
+    for _ in 0..50 {
+        z -= fnc(z) / (d_fnc(z) + 1e-5);
+    }
+
 
     1
 }
